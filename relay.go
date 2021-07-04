5dc6e530087e637c904767773cbdde73ca431bd0
zhaoya <marywangran@126.com>
2021-07-04 19:41:57 +0800

    Update relay.go

--- relay.go
+++ relay.go
@@ -11,37 +11,51 @@ import (
 func main() {
 	var host string
 	var port int
+	var mode int
 	var base int
 	flag.StringVar(&host, "h", "10.198.54.78", "host")
 	flag.IntVar(&port, "p", 1234, "port 1234")
+	flag.IntVar(&mode, "m", 0, "server|server-client")
 	flag.Parse()
 
 	base = port
-	listener, err := net.ListenTCP("tcp", &net.TCPAddr{
-		IP:   net.ParseIP(host),
-		Port: port,
-		Zone: "",
-	})
-	if err != nil {
-		log.Panic(err)
-	}
-	log.Printf("listen:%d\n", port)
-	for {
-		front, err := listener.Accept()
+	if mode == 0 {
+		listener, err := net.ListenTCP("tcp", &net.TCPAddr{
+			IP:   net.ParseIP(host),
+			Port: port,
+			Zone: "",
+		})
 		if err != nil {
-			log.Fatal(err)
+			log.Panic(err)
 		}
-		port = port + 1
-		log.Printf("front:%d  back:%d\n", base, port)
-		go handle_front(front, host, port)
+		log.Printf("listen:%d\n", port)
+		for {
+			front, err := listener.Accept()
+			if err != nil {
+				log.Fatal(err)
+			}
+			port = port + 1
+			log.Printf("front:%d  back:%d\n", base, port)
+			go handle_front(front, port)
+		}
+	} else {
+		front, err := net.DialTCP("tcp", nil, &net.TCPAddr {
+			IP:   net.ParseIP(host),
+			Port: port,
+			Zone: "",
+		})
+		if err != nil {
+			return
+		}
+		handle_front(front, port)
 	}
 }
 
-func handle_front(front net.Conn, addr string, port int) {
+func handle_front(front net.Conn, port int) {
 	defer front.Close()
 
 	blistener, err := net.ListenTCP("tcp", &net.TCPAddr{
-		IP:   net.ParseIP(addr),
+		IP:   net.ParseIP("0.0.0.0"),
 		Port: port,
 		Zone: "",
 	})
